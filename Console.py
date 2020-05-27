a578026bffbab61101d76c348d55b2cced494e3c
andresR0410 <af.romerog@uniandes.edu.co>
2020-05-27 13:20:23 -0500

    Código Interfaz Julito

--- Console.py
+++ Console.py
@@ -5,15 +5,11 @@ import WaveGenerator as WG
 ##
 import tkinter as tk
 
-
 window= tk.Tk()
 window.config(cursor="arrow")
 
-pf= tk.Label(
-	text= "Bienvenido a nuestro proyecto EGG"
-	foreground="white",  # Set the text color to white
-    background="black"  # Set the background color to blac
-)
+pf= tk.Label(text= "Bienvenido a nuestro proyecto ECG", foreground="white",  # Set the text color to white
+    background="black")  # Set the background color to black
 pf.pack()
 
 def salir():
@@ -39,21 +35,18 @@ parametros.place(x=15, y=20)
 parametros.config(bg="blue", width=300, height=140, relief=tk.GROOVE, bd=8)
 
 # FRAME DEL ECG
-ECG = tk.Frame(master=window).pack()
 ECG = tk.Frame(master=window)
 ECG.place(x=15, y=230)
 ECG.config(bg="yellow", width=300, height=150, relief=tk.RIDGE, bd=8)
 
 #FRAME PUNTOS ai bi
-puntos= tk.Frame(master=window).pack()
 puntos= tk.Frame(master=window)
-puntos.place(x=100,y=20)
+puntos.place(x=320,y=20)
 puntos.config(bg="white", width=300, height=150, relief=tk.RIDGE, bd=8)
 
 #FRAME MÉTODOS SOLUCION
-metodos= tk.Frame(master= window).pack()\
 metodos= tk.Frame(master=window)
-metodos.place(x=100,y=150)
+metodos.place(x=320,y=230)
 metodos.config(bg="red", width=300, height=150, relief=tk.RIDGE, bd=8)
 
 
@@ -92,18 +85,18 @@ Lat= tk.StringVar()
 FM= tk.StringVar()
 M= tk.StringVar()
 
-VFrCar = tk.Spinbox(master=parametros, from_=0, to=300, textvariable = FrCar, width = 5).place(x=150, y=40)
-FC = tk.Label(master=parametros, textvariable = FCRes,text="FC= ", command = obtener, width = 3).place(x=150, y=45)
+VFrCar = tk.Spinbox(master=parametros, from_=0, to=300, textvariable = FrCar, width = 5).place(x=100, y=20)
+FC = tk.Button(master=parametros, textvariable = FCRes,text="FC= ", command = obtener, width = 3).place(x=150, y=45)
 
-VLat = tk.Spinbox(master=parametros, from_=0, to=300, textvariable = NLatidos, width = 5).place(x=150, y=50)
-Latidos = tk.Label(master=parametros, textvariable = Lat,text="Lat= ", command = obtener, width = 3).place(x=150, y=55)
+VLat = tk.Spinbox(master=parametros, from_=0, to=300, textvariable = NLatidos, width = 5).place(x=150, y=20)
+Latidos = tk.Button(master=parametros, textvariable = Lat,text="Lat= ", command = obtener, width = 3).place(x=150, y=55)
 
 
-VFM = tk.Spinbox(master=parametros, from_=0, to=300, textvariable =FrMu , width = 5).place(x=150, y=60)
-FMu = tk.Label(master=parametros, textvariable = FM,text="FM= ", command = obtener, width = 3).place(x=150, y=65)
+VFM = tk.Spinbox(master=parametros, from_=0, to=300, textvariable =FrMu , width = 5).place(x=100, y=100)
+FMu = tk.Button(master=parametros, textvariable = FM,text="FM= ", command = obtener, width = 3).place(x=150, y=65)
 
-Morfo = tk.Spinbox(master=parametros, from_=0, to=300, textvariable =Morfo , width = 5).place(x=150, y=70)
-Morfologia = tk.Label(master=parametros, textvariable = M,text="FM= ", command = obtener, width = 3).place(x=150, y=75)
+Morfo = tk.Spinbox(master=parametros, from_=0, to=300, textvariable =Morfo , width = 5).place(x=150, y=100)
+Morfologia = tk.Button(master=parametros, textvariable = M,text="FM= ", command = obtener, width = 3).place(x=150, y=75)
 
 #ECG
 def ECG():
