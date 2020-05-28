047eb588eb3c4cc7e3591e805a1feb1d81ed79f4
Manuela <manunavarretes@gmail.com>
2020-05-28 16:22:53 -0500

    frecuencia cardiaca

--- Ecuaciones.py
+++ Ecuaciones.py
@@ -3,7 +3,28 @@ import scipy.optimize as opt
 import scipy.integrate as inte
 import numpy as np
 import math as mt
+##
+#encontrar picos para hallar frecuencia cardíaca desde el ECG
 
+def HR(frecuencia_muestreo):
+    time= """electrocardiograma""" / frecuencia_muestreo
+    peaks, properties = find_peaks(ecg, height=0.5, width=5)  # para encontrar solo las ondas R, cada latido
+    time_ecg = time[peaks]
+    time_ecg = time_ecg[1:0]
+
+    """plt.plot(time_ecg, ecg)
+    plt.plot(peaks/frecuencia_muestreo, ecg[peaks], "oc")  # peaks son indices dónde están los picos
+    plt.show()"""
+
+    # distancia entre picos
+    taco = np.diff(time_ecg)  # la diferencia en el tiempo
+    tacobpm = taco / 60  # paso de segundos a minutos
+
+    # la frecuencia se da:
+    HR = np.mean(tacobpm)
+    return HR
+
+##
 def dx(alpha, x, w, y):
     return alpha*x - w*y
 def dy(alpha, x, w, y):
